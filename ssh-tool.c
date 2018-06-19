ea170f6a0e9f5f42a5d2873719bdd9ae158f7a07
cheon <cheon0112358d@gmail.com>
2018-06-19 11:48:59 +0800

    [IMP]read file

--- ssh-tool.c
+++ ssh-tool.c
@@ -1,6 +1,7 @@
 # include<stdio.h>
 # include<stdlib.h>
 # include<string.h>
+# include <yaml.h>
 # include<sys/types.h>
 # include<signal.h>
 # include<ncurses.h>
@@ -11,6 +12,7 @@
 WINDOW *scrn;
 
 typedef struct {
+    char *groupname;
     char *hostname;
     char *ip;
     char *username;
@@ -18,8 +20,10 @@ typedef struct {
     char *intro;
 }host;
 
-host temp1={"solar", "104.128.226.70", "root", "**0x0112358D", "openvpn server"};
-host temp2={"eco", "49.4.2.233", "root", "Handhand300170", "eco server"};
+const host temp1={"all", "solar", "104.128.226.70", "root", "**0x0112358D", "openvpn server"};
+const host temp2={"all", "eco", "49.4.2.233", "root", "Handhand300170", "eco server"};
+
+host all[]={temp1,temp2};
 
 char cmdoutlines[MAXROW][MAXCOL];
 
@@ -42,17 +46,16 @@ void reverse(){
 void runpsax(){
     FILE *p;
     char ln[MAXCOL];
-    int row, tmp;
-    /** p = popen("ps ax", "r"); */
-    p = popen("cat ./server.txt", "r");
+    int row;
+    p = fopen("./server.txt", "r");
     for (row = 0; row < MAXROW; row++){
-        tmp = fgets(ln, MAXCOL, p);
-        if (tmp == NULL) break;
-        strncpy(cmdoutlines[row], ln, COLS);
-        cmdoutlines[row][MAXCOL-1] = 0;
+        if(fgets(ln, MAXCOL, p) != NULL){
+            strncpy(cmdoutlines[row], ln, COLS);
+            cmdoutlines[row][MAXCOL-1] = 0;
+        } else break;
     }
     ncmdlines = row;
-    pclose(p);
+    fclose(p);
 }
 
 void showlastpart(){
@@ -67,7 +70,6 @@ void showlastpart(){
         nwinlines = LINES;
     }
     cmdlastrow = cmdstartrow + nwinlines - 1;
-    printw("================================================================\n");
     for (row = cmdstartrow, winrow = 0; row <= cmdlastrow; row++, winrow++)
         mvaddstr(winrow, 0, cmdoutlines[row]);
     refresh();
@@ -79,7 +81,9 @@ void updown(int inc){
     int tmp = winrow + inc;
     if (tmp >= 0 && tmp < LINES) {
         mvaddstr(winrow, 0, cmdoutlines[cmdstartrow+winrow]);
-        winrow = tmp;
+        if(tmp <= cmdlastrow && tmp >= cmdstartrow+2){
+            winrow = tmp;
+        }
         reverse();
     }
 }
@@ -90,27 +94,21 @@ void rerun(){
 }
 
 void connect(){
+    refresh();
     endwin();
-    char *temp[] = {"sshpass -p ", temp1.hostname, " ssh ", temp1.username, "@", temp1.hostname};
-    char *result=NULL;
-    for(int i=0; i<6; i++){
-        printf("%s\n", temp[i]);
+    char *temp[] = {"sshpass -p ", temp2.password, " ssh ", temp2.username, "@", temp2.ip};
+    char *result;
+    result = (char *)malloc(100*sizeof(char));
+    result[0]='\0';
+    int i;
+    for(i=0; i<6; i++){
         strcat(result, temp[i]);
     }
+    printf("connect to %s...\n", temp1.hostname);
     printf("%s\n", result);
-    //system("sshpass -p ssh root@49.4.2.233");
-    getchar();
+    system(result);
+    free(result);
     rerun();
-}
-
-void prockill(){
-    char *pid;
-    pid = strtok(cmdoutlines[cmdstartrow+winrow], " ");
-    /** kill(atoi(pid),9); */
-    rerun();
-    printf("=============================================\n");
-    printf("%s\n", pid);
-    printf("=============================================\n");
 }
 
 int main(){
